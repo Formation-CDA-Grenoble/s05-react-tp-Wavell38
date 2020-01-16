605657294bc405aec93b8048ef0c6511e799dcb4
Vincent <vincent.grange38@gmail.com>
2020-01-16 10:25:17 +0100

    CardOeuvre and init commit typescript update

---
+++ src/Components/CardOeuvre.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default class CardOeuvre extends React.Component<any, any, any> {
+
+	public constructor(props:any) {
+		super(props)
+	}
+
+	render() {
+		let img:string|null = this.props.image? this.props.image : ""
+		return(
+			<div className="card-oeuvre">
+				<div className="title" style={{ background: `url(${img})` }}>
+					<p>{ this.props.author }</p>
+					<h2>{ this.props.title }</h2>
+				</div>
+				<div className="price">
+					<p>{ this.props.price }</p>
+				</div>
+			</div>
+		)
+	}
+}
